70f4435d4838291df853012b6e97229c261e41b1
angetraore-dev <sonya.traore@ymail.com>
2025-05-14 12:20:01 +0000

    form-group component is Done

--- resources/views/components/form-group.blade.php
+++ resources/views/components/form-group.blade.php
@@ -7,14 +7,16 @@ $class ??=null;
 @endphp
 <!-- form-group est en grid: flex flex-col w-full md:grid grid-cols-1 mb-4 -->
 
-<div @class(['form-group'])>
+<div @class(['form-group', $class])>
+
     <label class="label" for="{{ $name }}">{{ $label }}</label>
+
     @if($type == 'textarea')
         <textarea class="form-control" name="{{$name}}" id="{{$name}}" class="form-control">
-        {{old($value, '')}}
-    </textarea>
+            {{old($name, $value)}}
+        </textarea>
     @else
-        <input type="{{$type}}" name="{{$name}}" id="{{$name}}" class="form-control" value="{{old($value, '')}}" />
+        <input type="{{$type}}" name="{{$name}}" id="{{$name}}" class="form-control" value="{{old($name, $value)}}" />
     @endif
     <p @class(['text-sm text-danger'])>
         @error($name)
